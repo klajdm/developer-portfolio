6e32d3012f39c6c208f987c809ff07dd2771d7e1
klajdm <klajdimurati3@gmail.com>
2023-08-07 22:11:23 +0200

    Implemented Sanity Studio for receiving data from the backend

--- next.config.js
+++ next.config.js
@@ -4,3 +4,10 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.SANITY_DATASET,
+  },
+};
